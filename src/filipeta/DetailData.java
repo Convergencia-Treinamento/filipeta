9a1b681f4ee91f38fc112011bc2266ab518a4fed
AndreLPD <andre_luis_pessoa@hotmail.com>
2017-03-14 17:48:55 -0300

    continuacao filipeta

---
+++ src/filipeta/DetailData.java
@@ -0,0 +1,14 @@
+package filipeta;
+
+public class DetailData {
+	
+	private Object data;
+	
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+}
--- src/filipeta/FilipetaTest.java
+++ src/filipeta/FilipetaTest.java
@@ -1,5 +1,6 @@
 package filipeta;
 
+import java.io.Console;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -11,9 +12,7 @@ import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
-
-
-
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
 
 public class FilipetaTest {
@@ -24,9 +23,42 @@ public class FilipetaTest {
 		
 		filipeta.put("nome", "Morgana Matheus M Andrade");
         
-		Map<String, Object> mapa = new HashMap<String, Object>();
-		List<Map<String, Object>> itens = new ArrayList<Map<String,Object>>();
-        
+		List<DetailData> itens = new ArrayList<>();
+		
+		Map<String, Map<String, Object>> linha = new HashMap<String, Map<String,Object>>();
+		Map<String, Object> esquerda = new HashMap<String, Object>();
+		esquerda.put("txDesconto", 10);
+		esquerda.put("linhaCategoria", "categoria 2");
+		esquerda.put("linha1", "remedios");
+		esquerda.put("linha2", "teste2");
+		linha.put("esquerda", esquerda);
+		
+		Map<String, Object> direita = new HashMap<String, Object>();
+		direita.put("txDesconto", 15);
+		direita.put("linhaCategoria", "categoria 3");
+		direita.put("linha1", "conveniência");
+		direita.put("linha2", "teste3");
+		linha.put("direita", direita);
+		
+		esquerda.put("txDesconto1", "10");
+		esquerda.put("linhaCategoria1", "categoria 4");
+		esquerda.put("linha3", "cosmeticos");
+		esquerda.put("linha4", "teste4");
+		
+		direita.put("txDesconto1", 10);
+		direita.put("linhaCategoria1", "categoria 5");
+		direita.put("linha3", "caixa");
+		direita.put("linha4", "teste3");
+		linha.put("direita", direita);
+		
+		DetailData dd = new DetailData();
+		dd.setData(linha);
+		itens.add(dd);
+		
+		for (DetailData detailData : itens) {
+			System.out.println(detailData.getData());
+		}
+       
 		Map<String, Object> destaqueCima = new HashMap<String, Object>();
 		destaqueCima.put("txDesconto", "20");
 		destaqueCima.put("linhaCategoria", "categoria 1");
@@ -34,42 +66,53 @@ public class FilipetaTest {
 		destaqueCima.put("linha2", "teste1");
 		filipeta.put("destaqueCima", destaqueCima);
 
-		Map<String, Object> item2 = new HashMap<String, Object>();
+		/*//Map<String, Object> item2 = new HashMap<String, Object>();
+		List<Map<String,Object>> detail = new ArrayList<Map<String,Object>>();
 		Map<String, Object> linha2 = new HashMap<String, Object>();
-		itens.add(item2);
+		Map<String, Object> item2 = new HashMap<String, Object>();
+		detail.add(item2);
 		item2.put("txDesconto", "10");
 		item2.put("linhaCategoria", "categoria 2");
 		linha2.put("linha1", "remedios");
 		linha2.put("linha2", "teste2");
 		item2.put("linhaMarca2", linha2);
+		filipeta.put("detail", item2);
+		
+		
 
 		Map<String, Object> item3 = new HashMap<String, Object>();
 		Map<String, Object> linha3 = new HashMap<String, Object>();
-		itens.add(item3);
+		detail.add(item3);
 		item3.put("txDesconto", "15");
 		item3.put("linhaCategoria", "categoria 3");
 		linha3.put("linha1", "conveniência");
 		linha3.put("linha2", "teste3");
 		item3.put("linhaMarca3", linha3);
+		filipeta.put("detail", item3);
 
 		Map<String, Object> item4 = new HashMap<String, Object>();
 		Map<String, Object> linha4 = new HashMap<String, Object>();
-		itens.add(item4);
+		detail.add(item4);
 		item4.put("txDesconto", "10");
 		item4.put("linhaCategoria", "categoria 4");
 		linha4.put("linha1", "cosmeticos");
 		linha4.put("linha2", "teste4");
 		item4.put("linhaMarca4", linha4);
+		filipeta.put("detail", item4);
 
 		Map<String, Object> item5 = new HashMap<String, Object>();
 		Map<String, Object> linha5 = new HashMap<String, Object>();
-		itens.add(item5);
+		detail.add(item5);
 		item5.put("txDesconto", "10");
 		item5.put("linhaCategoria", "categoria 5");
 		linha5.put("linha1", "caixa");
 		linha5.put("linha2", "teste5");
 		item5.put("linhaMarca5", linha5);
+		filipeta.put("detail", item5);
 		
+		for (Map<String, Object> map : detail) {
+		System.out.println(map);
+		}*/
 		
 		//List<Map<String, Object>> ofertas = new ArrayList<Map<String,Object>>();
 
@@ -93,13 +136,19 @@ public class FilipetaTest {
 		validade1.put("validade2", "04/12/2016");
 		filipeta.put("validades", validade1);
         
-		System.out.println(filipeta);
+		
 		
 		Map<String, Object> parametros = new HashMap<String, Object>();
 		parametros.put("filipeta", filipeta);
+		//System.out.println(detail.get(0));
+		
+		//System.out.println(parametros);
+		
+		
+		JRBeanCollectionDataSource jrbcd = new JRBeanCollectionDataSource(itens);
 		// compilacao do JRXML
 		JasperReport report = JasperCompileManager.compileReport("filipeta.jrxml");
-		JasperPrint print = JasperFillManager.fillReport(report, parametros);
+		JasperPrint print = JasperFillManager.fillReport(report, parametros, jrbcd);
 		JasperExportManager.exportReportToPdfFile(print, "filipetagerada.pdf");
 		System.out.println("Relatório gerado.");
 		
